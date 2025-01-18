04dab37cf9bd562f4ac742a2f2d4474ecc3636ab
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-01-18 14:42:22 +0200

    First Project Q1)

---
+++ Demo/3DPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    #region 1.Define 3D Point Class and the basic Constructors (use chaining in constructors).
+    class Point3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+
+        public Point3D(int x)
+        {
+            this.X = x;
+        }
+        public Point3D(int x, int y) : this(x)
+        {
+            this.Y = y;
+        }
+        public Point3D(int x, int y, int z) : this(x, y)
+        {
+            this.Z = z;
+        }
+    } 
+    #endregion
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region First Project:
+
+
+            #endregion
+
         }
     }
 }
