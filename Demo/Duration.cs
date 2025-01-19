dbbdf4a2a4b66acd52642591f456ad94c58fa380
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-01-19 09:20:19 +0200

    Third Project:1)

---
+++ Demo/Duration.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Demo
+{
+
+    #region 1.	Define Class Duration To include Three Attributes Hours, Minutes and Seconds.
+    internal class Duration
+    {
+
+        public int? Hours { get; set; }
+        public int? Minutes { get; set; }
+        public int? Seconds { get; set; }
+
+        public Duration()
+        {
+            
+        }
+
+        public Duration(int? Hours, int? Minutes, int? Seconds)
+        {
+            this.Hours = Hours;
+            this.Minutes = Minutes;
+            this.Seconds = Seconds;
+        }
+        public Duration(int totalSeconds)
+        {
+            this.Hours = totalSeconds / 3600;
+            int remainingSeconds = totalSeconds % 3600;
+            this.Minutes = remainingSeconds / 60;
+            this.Seconds = remainingSeconds % 60;
+        }
+
+    }
+    #endregion
+}
