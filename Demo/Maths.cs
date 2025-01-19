51b0058facc20b1a03e48447a44c5746bb797cb4
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-01-19 09:18:03 +0200

    Second Project:

---
+++ Demo/Maths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Maths
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+
+        public static int Add(int x , int y) {
+        return x + y;
+        }
+        public static int Sub(int x , int y) {
+            return x - y;
+        }
+        public static int Mul(int x , int y) {
+            return x * y;
+        }
+        public static int Div(int x , int y) {
+            return  y==0?0 :  x / y;
+        }
+    }
+}
