c312ba268d5139a23ef7409fe301f3ceff89faa3
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-01-19 09:22:46 +0200

    Third Project:4)

--- Demo/Duration.cs
+++ Demo/Duration.cs
@@ -44,10 +44,114 @@ namespace Demo
         }
         #endregion
 
+        #region 4.	Implement All required Operators overloading to enable this Code:
+        //●	D3=D1+D2
+        public static Duration operator +(Duration left, Duration right)
+        {
+
+            return new Duration()
+            {
+                Hours = left.Hours + right.Hours,
+                Minutes = left.Minutes + right.Minutes,
+                Seconds = left.Seconds + right.Seconds,
+            };
+        }
+
+        //●	D3=D1 + 7800
+        public static Duration operator +(Duration left, int right)
+        {
+            int totalSeconds = (left.Hours ?? 0) * 3600 + (left.Minutes ?? 0) * 60 + (left.Seconds ?? 0) + right;
+            int remainingSeconds = totalSeconds % 3600;
+
+            return new Duration()
+            {
+                Hours = totalSeconds / 3600,
+                Minutes = remainingSeconds / 60,
+                Seconds = remainingSeconds % 60,
+            };
+        }
+        //●	D3=666+D3
+        public static Duration operator +(int left, Duration right)
+        {
+            int totalSeconds = (right.Hours ?? 0) * 3600 + (right.Minutes ?? 0) * 60 + (right.Seconds ?? 0) + left;
+            int remainingSeconds = totalSeconds % 3600;
+
+            return new Duration()
+            {
+                Hours = totalSeconds / 3600,
+                Minutes = remainingSeconds / 60,
+                Seconds = remainingSeconds % 60,
+            };
+        }
+
+
+        //●	D3 = ++D1(Increase One Minute)
+
+        public static Duration operator ++(Duration right)
+        {
+
+
+            return new Duration()
+            {
+                Hours = right.Hours,
+                Minutes = right.Minutes + 1,
+                Seconds = right.Seconds,
+            };
+        }
+        //●	D3 = --D2(Decrease One Minute)
+        public static Duration operator --(Duration right)
+        {
+
+
+            return new Duration()
+            {
+                Hours = right.Hours,
+                Minutes = right.Minutes - 1,
+                Seconds = right.Seconds,
+            };
+        }
+        //●	D1= D1 -D2
+
+        public static Duration operator -(Duration left, Duration right)
+        {
+
+            return new Duration()
+            {
+                Hours = left.Hours - right.Hours,
+                Minutes = left.Minutes - right.Minutes,
+                Seconds = left.Seconds - right.Seconds,
+            };
+        }
+
+        //●	If (D1>D2)
+        public static bool operator >(Duration left, Duration right)
+        {
+            return (left.Hours * 3600 + left.Minutes * 60 + left.Seconds) > (right.Hours * 3600 + right.Minutes * 60 + right.Seconds) ? true : false;
+
+        }
+
+        //●	If(D1<=D2
+        public static bool operator <(Duration left, Duration right)
+        {
+            return (left.Hours * 3600 + left.Minutes * 60 + left.Seconds) < (right.Hours * 3600 + right.Minutes * 60 + right.Seconds) ? true : false;
+
+        }
+        //●	If (D1)
+        public static implicit operator bool(Duration d)
+        {
+            return (d.Hours ?? 0) > 0 || (d.Minutes ?? 0) > 0 || (d.Seconds ?? 0) > 0;
+        }
+        //●	DateTime Obj = (DateTime) D1
+        public static explicit operator DateTime(Duration D)
+        {
+            return new DateTime(hour: D.Hours ?? 0, minute: D.Minutes ?? 0, second: D.Seconds ?? 0, year: 2024, month: 12, day: 12);
+
+        }
+
+        #endregion
 
 
 
-  
     }
     #endregion
 }
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -92,7 +92,41 @@
             #endregion
 
 
-      
+            #region 4.	Implement All required Operators overloading to enable this Code:
+            //●	D3=D1+D2
+            //Duration D1 = new Duration(1, 10, 15);
+            //Duration D2 = new Duration(7800);
+            ////Duration D3=D1+ D2;
+            //Console.WriteLine(D1);
+            //Console.WriteLine(D2);
+            //Console.WriteLine(D3);
+
+            ////●	D3=D1 + 7800
+            ////D3 = D1 + 7800;
+
+            ////Console.WriteLine(D3);
+            ////●	D3=666+D3
+            ////D3 = 666 + D3;
+            ////Console.WriteLine(D3);
+
+            ////●	D3 = ++D1(Increase One Minute)
+
+            //D3 = ++D1;
+            //Console.WriteLine(D3);
+            ////●	D3 = --D2(Decrease One Minute)
+            //D3 = --D1;
+            //Console.WriteLine(D3);
+            ////●	D1= D1 -D2
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1);
+            //●	If(D1 > D2)
+            //Console.WriteLine(D1 > D2);
+            //  ●	If(D1 <= D2)
+            //Console.WriteLine(D1 < D2);
+            //●	DateTime Obj = (DateTime) D1
+            //DateTime Obj = (DateTime)D1;
+            //Console.WriteLine(Obj);
+            #endregion
         }
     }
 }
