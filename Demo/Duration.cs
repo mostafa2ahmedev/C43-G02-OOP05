7db98f197f122b95d318867a835e8667c4db1ee6
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-01-19 09:20:49 +0200

    Third Project:2)

--- Demo/Duration.cs
+++ Demo/Duration.cs
@@ -29,7 +29,25 @@ namespace Demo
             this.Minutes = remainingSeconds / 60;
             this.Seconds = remainingSeconds % 60;
         }
+        #region 2.	Override All System.Object Members (ToString, Equals,GetHasCode) 
+        public override string ToString()
+        {
+            return $"Hours: {Hours},Minutes; {Minutes ?? 0},Seconds: {Seconds ?? 0}";
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+        #endregion
 
+
+
+
+  
     }
     #endregion
 }
